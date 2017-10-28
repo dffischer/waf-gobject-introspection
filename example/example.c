232c236ecede373217579f47bd4153d92e8234ce
XZS <d.f.fischer@web.de>
2017-10-28 09:20:46 +0100

    Build a small shared library as an example
    It is written with its introspected future in mind.

---
+++ example/example.c
@@ -0,0 +1,6 @@
+#include "example.h"
+#include <stdio.h>
+
+void example_greet(char *gretee) {
+  printf("Hello, %s!\n", gretee);
+}
---
+++ example/example.h
@@ -0,0 +1,12 @@
+#ifndef __EXAMPLE_H__
+#define __EXAMPLE_H__
+
+/**
+ * example_greet:
+ * @gretee: who to greet
+ *
+ * Emits a greeting message, directed at a given entity.
+ */
+void example_greet(char *gretee);
+
+#endif /* __EXAMPLE_H__ */
