a188172d77b9739a6cf07dc64a2eed38c17df08b
XZS <d.f.fischer@web.de>
2017-11-04 16:06:57 +0100

    Allow integrating gir feature into compilation
    Introspection data is often built and shipped with the library it
    describes. So it is an often useful shortcut to direct both together.
    Finding the link_task is actually easier when it is readily available in
    the current generator.
    
    As linker processes can be various, it is assumed that a link_task will
    generate a product introspection can depend upon.

--- gir.py
+++ gir.py
@@ -4,21 +4,32 @@
 Create and install GObject Introspection data.
 
 The functions herein center around a feature called 'gir' which scans source
-files for GObject Introspection data and compiles a Typelib.
+files for GObject Introspection data and compiles a Typelib. It and all
+mandatory parameters can simply be added to a generator creating a shared
+library from c sources or used to create a separate task generator that
+references the target library.
 
     def configure(cnf):
         cnf.load("gir")
 
     def build(bld):
+        bld(features="c cshlib gir",
+                source="object.c",  # main code to be compiled
+                target="object",
+                scan="object.h",  # header files for the g-ir-scanner
+                namespace="Object",
+                version=1)  # defaults to 0
+
+or
+
         bld(features="c cshlib",  # library compilation
                 source="object.c", target="object", use="GLIB2")
         bld(features="gir", namespace="Object",
                 lib="object",  # library to introspect
-                scan="object.h",  # header files to scan
-                version=1)  # defaults to 0
+                scan="object.h")  # header files to scan
 """
 
-from waflib.TaskGen import feature
+from waflib.TaskGen import feature, after_method
 from waflib.Task import Task
 from waflib.Errors import WafError
 
@@ -44,6 +55,7 @@ class gircompile(Task):
     run_str = "${G_IR_COMPILER} -o ${TGT} ${SRC}"
 
 @feature("gir")
+@after_method('apply_link')
 def process_gir(gen):
     namespace = getattr(gen, "namespace", None)
     if not namespace:
@@ -57,11 +69,16 @@ def process_gir(gen):
     env.NAMESPACE = namespace
     env.VERSION = version
 
-    try:
+    lib = getattr(gen, "lib", None)
+    if lib:
         lib_gen = gen.bld.get_tgen_by_name(gen.lib)
-    except AttributeError as e:
-        raise WafError(f"{gen} lacks a library to introspect") from e
-    lib_task = lib_gen.link_task
+        lib_task = lib_gen.link_task
+    else:
+        lib_task = getattr(gen, 'link_task', None)
+        if not lib_task:
+            raise WafError(f"{gen} lacks a library to introspect "
+                    "and does not build one itself")
+        lib_gen = gen
     env.append_value('GIRLIB', [lib_gen.target])
     scan_task.dep_nodes.extend(lib_task.outputs)
     env.append_unique('GIRPATH', [
